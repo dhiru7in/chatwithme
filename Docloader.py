0ec5a8d371b29d72aa29dda085704b6fe462b6c7
dhirendra_singh <92597609+dhiru7in@users.noreply.github.com>
2026-06-08 00:38:45 +0530

    Update Docloader.py

--- Docloader.py
+++ Docloader.py
@@ -6,7 +6,7 @@ import streamlit as st
 import tempfile
 
 
-api_key = st.secrets["AQ.Ab8RN6LofsF8EjVSPgF6e4e76ZKRuKyTeqOxskF8aLA-cmyiRg"]
+api_key = st.secrets["GOOGLE_API_KEY"]
 
 st.set_page_config(
     page_title="PDF Chatbot with Gemini",
